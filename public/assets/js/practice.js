def243e4c194661e9487f17f1bc78ff4c9cef3b6
Savannah Kolb <savannahnkolb@gmail.com>
2021-07-05 23:29:10 -0400

    started handlebars, added css and js files for map, edited routes to allow homepage to display on load, added working html page for map because map in handlebars is not working properly yet.

---
+++ public/assets/js/practice.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    
+    // CSSMap;
+    $("#map-continents").CSSMap({
+      "size": 1450,
+      "tooltips": "floating-top-center",
+      "responsive": "auto",
+      "mobileSupport": true
+    });
+    // END OF THE CSSMap;
+  });
+  // Window.Open Option click event 
+      // let africa = () => {
+      //     window.open("")
+      // }
+      
+      // africa();
+
+// NOT A BUTTON 
+// const africaUrl = './africa.html'
+
+// //event listener when "Lets begin" button is pressed and user gets redirected to the input page
+
+// $('#africa').on('click', function () {
+//   document.location.replace(africaUrl)
+// })
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,75 +2,68 @@ const router = require('express').Router();
 
 module.exports = (db) => {
   // Load register page
-  router.get('/register', (req, res) => {
-    if (req.isAuthenticated()) {
-      res.redirect('/profile');
-    } else {
-      res.render('register');
-    }
-  });
+  // router.get('/register', (req, res) => {
+  //   if (req.isAuthenticated()) {
+  //     res.redirect('/profile');
+  //   } else {
+  //     res.render('register');
+  //   }
+  // });
 
-  // Load profile page
-  router.get('/profile', (req, res) => {
-    if (req.isAuthenticated()) {
-      db.User.findOne({
-        where: {
-          id: req.session.passport.user.id
-        }
-      }).then(() => {
-        const user = {
-          userInfo: req.session.passport.user,
-          isloggedin: req.isAuthenticated()
-        };
-        // console.log(user);
-        res.render('profile', user);
+  // // Load profile page
+  // router.get('/profile', (req, res) => {
+  //   if (req.isAuthenticated()) {
+  //     db.User.findOne({
+  //       where: {
+  //         id: req.session.passport.user.id
+  //       }
+  //     }).then(() => {
+  //       const user = {
+  //         userInfo: req.session.passport.user,
+  //         isloggedin: req.isAuthenticated()
+  //       };
+  //       // console.log(user);
+  //       res.render('profile', user);
+  //     });
+  //   } else {
+  //     res.redirect('/');
+  //   }
+  // });
+
+  // // Load dashboard page
+  // router.get('/', (req, res) => {
+  //   if (req.isAuthenticated()) {
+  //     const user = {
+  //       user: req.session.passport.user,
+  //       isloggedin: req.isAuthenticated()
+  //     };
+  //     res.render('dashboard', user);
+  //   } else {
+  //     res.render('dashboard');
+  //   }
+  // });
+
+  // // Load dashboard page
+  // router.get('/dashboard', (req, res) => {
+  //   if (req.isAuthenticated()) {
+  //     const user = {
+  //       user: req.session.passport.user,
+  //       isloggedin: req.isAuthenticated()
+  //     };
+  //     res.render('dashboard', user);
+  //   } else {
+  //     res.render('dashboard');
+  //   }
+  // });
+
+  // Load HOME PAGE
+  router.get('/', async (req, res) => {
+    res.render('homepage');
+    });
+
+  router.get('/africa', async (req, res) => {
+    res.render('africa');
       });
-    } else {
-      res.redirect('/');
-    }
-  });
-
-  // Load dashboard page
-  router.get('/', (req, res) => {
-    if (req.isAuthenticated()) {
-      const user = {
-        user: req.session.passport.user,
-        isloggedin: req.isAuthenticated()
-      };
-      res.render('dashboard', user);
-    } else {
-      res.render('dashboard');
-    }
-  });
-
-  // Load dashboard page
-  router.get('/dashboard', (req, res) => {
-    if (req.isAuthenticated()) {
-      const user = {
-        user: req.session.passport.user,
-        isloggedin: req.isAuthenticated()
-      };
-      res.render('dashboard', user);
-    } else {
-      res.render('dashboard');
-    }
-  });
-
-  // Load example index page
-  router.get('/example', function (req, res) {
-    if (req.isAuthenticated()) {
-      db.Example.findAll({ where: { UserId: req.session.passport.user.id }, raw: true }).then(function (dbExamples) {
-        res.render('example', {
-          userInfo: req.session.passport.user,
-          isloggedin: req.isAuthenticated(),
-          msg: 'Welcome!',
-          examples: dbExamples
-        });
-      });
-    } else {
-      res.redirect('/');
-    }
-  });
 
   // Load example page and pass in an example by id
   router.get('/example/:id', function (req, res) {
