5e7ee7e7076f61f4fcf9cee9e03584279cc586a0
lissy <slissy513@gmail.com>
2021-07-11 13:58:40 -0400

    last minute comment additions; revised syntax errors; added to userRoutes

--- routes/homepageRoutes.js
+++ routes/homepageRoutes.js
@@ -1,4 +1,7 @@
 const router = require('express').Router();
+// will need to add more 
 const { Continent } = require('../models');
 // Import the custom middleware
 const withAuth = require('../auth');
+//routes for continent
+router.get
