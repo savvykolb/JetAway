8964e9fb66294000348e641cedf79cb356930525
Savannah Kolb <savannahnkolb@gmail.com>
2021-07-15 15:16:38 -0400

    changed home route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -48,7 +48,7 @@ const withAuth = require('../utils/auth');
     });
 
 // Display Homepage of Site
-  router.get('/homepage', async(req, res) => {
+  router.get('/', async(req, res) => {
   res.render('homepage');
     });
 
